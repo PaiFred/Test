67ba22ae5498d72071f233ef00510c59236f5107
PAIF <paif@student.unimelb.edu.au>
2020-10-20 12:24:06 +1100

    remove

--- api/src/main/java/com/medsec/util/GScommands.java
+++
@@ -1,5 +0,0 @@
-package com.medsec.util;
-@Deprecated
-public enum GScommands {
-    AUTHENTICATION, APPOINTMENT, PATIENT, FILE, DISCONNECTION;
-}
--- frontend/lib/util/serverDetails.dart
+++
@@ -1,5 +0,0 @@
-class ServerDetails{
-  static String ip="http://13.58.243.191";
-  static String port="8080";
-  static String api="/medsec/api/";
-}
